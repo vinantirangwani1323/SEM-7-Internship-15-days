64255a258bcebb801c256934c467fc8526a8bb45
Rangwani Vinanti <231260116052setiit@gmail.com>
2026-07-07 16:02:19 +0530

    Day2 Internship task

---
+++ day2.py
@@ -0,0 +1,19 @@
+#Single line comment
+'''Multi lines comment
+'''
+a = float(input("Enter No:"))
+b = float(input("Enter No:"))
+c = a + b
+print(int(c))
+print("A value is ",a, "B value is ",b, "C value is ",c)
+print("A value is {}, B value is {}, C value is {}".format(a, b, c))
+print(f"A value is {a}, B value is {b}, C value is {c}")
+
+d = 11
+if d == 11:
+ print("d is 11")
+elif d > 11:
+ print("d > 11")
+else:
+ print("d < 11")
+
