1f946c636193c48e21e6384c5a49ac310f1098ef
Rangwani Vinanti <231260116052setiit@gmail.com>
2026-07-12 14:39:46 +0530

    string methods

---
+++ string_methods.py
@@ -0,0 +1,60 @@
+
+
+mystr = "hello vinanti"
+mystr2 = ("hello","vinanti")
+mystr1 = "Rangwani"
+myans1 = mystr.capitalize()
+myans2 = mystr.title()
+myans3 = mystr.swapcase()
+myans4 = mystr.casefold()
+myans5 = mystr.isalpha()
+myans6 = mystr.isalnum()
+myans7 = mystr.isnumeric()
+myans8 = mystr.endswith("vinanti")
+myans9 = mystr.center(25)
+myans10 = mystr.find("h")
+myans11 = mystr.index('v')
+myans12 = mystr.islower()
+myans13 = mystr.title()
+myans15 = mystr.isupper()
+myans16 = " ".join(mystr2)
+myans17 = mystr1.lower()
+myans18 = mystr.upper()
+print(myans1)
+print(myans2)
+print(myans3)
+print(myans4)
+print(myans5)
+print(myans6)
+print(myans7)
+print(myans8)
+print(myans9)
+print(myans10)
+print(myans11)
+print(myans12)
+print(myans13)
+print(myans15)
+print(myans16)
+print(myans17)
+print(myans18)
+
+
+str = "Hello Hiii Hello Hiii Hello"
+ans = str.count("Hello")
+print(ans)
+st1 = "V\ti\tn\ta\tn\tt\t\ti\t"
+ans1 = st1.expandtabs()
+print(ans)
+
+st2 = "How are you"
+ans3 = st2.partition("are")
+print(ans3)
+st3 = "Hello world" 
+ans4 = st3.replace("Hello", "Hi")
+print(ans4)
+st4 = "Hello vinati"
+ans5 = st4.startswith("Hello")
+print(ans5)
+st = "Hello Hiii Hello Hiii Hello"
+an = st.split()
+print(an)
